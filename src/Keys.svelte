30607f344f453df78926b5e1b52f692151687275
Kirill Maksimov <kirill@orbs.com>
2019-07-17 11:38:35 +0300

    Expose privatekey

---
+++ src/Keys.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let privateKey, address;
+  let showPrivateKey = false;
+
+  const toggleShowPrivateKey = () => {
+    showPrivateKey = !showPrivateKey;
+  }
+</script>
+
+<style>
+    .address {
+        font-family: 'Courier New', Courier, monospace
+    }
+</style>
+
+<div>
+Your address is <span class="address">{address}</span>
+<button on:click={toggleShowPrivateKey}>{#if showPrivateKey}Hide private key{:else}Show private key{/if}</button>
+<br/>
+{#if showPrivateKey}
+<p class="address">{privateKey}</p>
+{/if}
+</div>
